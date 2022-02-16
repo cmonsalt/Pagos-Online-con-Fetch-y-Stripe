545b42f37e1d91582940dc52798ccaafba34a21d
cmonsalt <cristiantm12.com>
2022-02-15 19:03:55 -0500

    Pagos Online con Fetch y Strip 2/3

--- js/stripe-checkout.js
+++ js/stripe-checkout.js
@@ -3,30 +3,42 @@ import STRIPE_KEYS from "./stripe-keys.js";
 const d = document,
   $camisetas = d.getElementById("camisetas"),
   $template = d.getElementById("camiseta-id").content,
-  $fragment = d.createDocumentFragment();
+  $fragment = d.createDocumentFragment(),
+  fetchOptions = {
+    headers: {
+      Authorization: `Bearer ${STRIPE_KEYS.secret}`,
+    },
+  };
 
-fetch("https://api.stripe.com/v1/products", {
-  headers: {
-    Authorization: `Bearer ${STRIPE_KEYS.secret}`,
-  },
-})
-  .then((res) => {
-    //console.log(res);
-    return res.json();
-  })
+let prices, products;
+Promise.all([
+  fetch("https://api.stripe.com/v1/products", fetchOptions),
+  fetch("https://api.stripe.com/v1/prices", fetchOptions),
+])
+  .then((responses) => Promise.all(responses.map((res) => res.json())))
   .then((json) => {
-    console.log(json);
-  });
+    //console.log(json);
+    products = json[0].data;
+    prices = json[1].data;
+    //console.log(products, prices);
 
-fetch("https://api.stripe.com/v1/prices", {
-  headers: {
-    Authorization: `Bearer ${STRIPE_KEYS.secret}`,
-  },
-})
-  .then((res) => {
-    //console.log(res);
-    return res.json();
+    prices.forEach((el) => {
+      let productData = products.filter((product) => product.id === el.product);
+      //console.log(productData);
+      $template.querySelector(".camiseta").setAttribute("data.price", el.id);
+      $template.querySelector("img").src = productData[0].images[0];
+      $template.querySelector("img").alt = productData[0].name;
+      $template.querySelector("figcaption").innerHTML = ` 
+      ${productData[0].name}
+      <br>
+      ${el.unit_amount_decimal} ${el.currency}
+      `;
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+    $camisetas.appendChild($fragment);
   })
-  .then((json) => {
-    console.log(json);
+  .catch((err) => {
+    let message = err.statusText || "Ocurrio un error";
+    $camisetas.innerHTML = ` <p>Error ${err.status}: ${message}</p>`;
   });
